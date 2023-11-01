6a59aafe9f104b39fce7e2fb58f45beac8a16b06
ybsolomon <ybsolomon@dons.usfca.edu>
2023-11-01 16:32:32 -0700

    done with forward

--- HMM.py
+++ HMM.py
@@ -7,19 +7,21 @@ import os
 import numpy
 import random
 
-# observations
-class Observation:
-    def __init__(self, stateseq, outputseq):
-        self.stateseq  = stateseq   # sequence of states
-        self.outputseq = outputseq  # sequence of outputs
-    def __str__(self):
-        return ' '.join(self.stateseq)+'\n'+' '.join(self.outputseq)+'\n'
-    def __repr__(self):
-        return self.__str__()
-    def __len__(self):
-        return len(self.outputseq)
-
 # hmm model
+def getObservations(filename):
+    observations = []
+    file = open(filename, 'r')
+    line = file.readline()
+
+    while line:
+        if len(line) > 1:
+            observations.append(['#'] + line.split())
+
+        line = file.readline()
+
+    return observations
+
+
 class HMM:
     def __init__(self, transitions={}, emissions={}):
         """creates a model from transition and emission probabilities"""
@@ -41,13 +43,14 @@ class HMM:
         while line:
             words = line.split()
             if not words[0] in self.transitions:
-                self.transitions[words[0]] = {}
+                self.transitions[words[0]] = {'#': 0.0}
 
             self.transitions[words[0]][words[1]] = float(words[2])
             line = trans.readline()
 
         trans.close()
 
+        self.emissions['#'] = {}
         emit = open(basename + '.emit', 'r')
         line = emit.readline()
         while line:
@@ -56,9 +59,12 @@ class HMM:
                 self.emissions[words[0]] = {}
 
             self.emissions[words[0]][words[1]] = float(words[2])
+            self.emissions['#'][words[1]] = 0.0
             line = emit.readline()
 
         emit.close()
+
+        print(self.transitions.keys())
 
 
    ## you do this.
@@ -79,30 +85,84 @@ class HMM:
 
             current = random_state
 
-        return states, emissions
+        return emissions
 
     def forward(self, observation):
-        """given an observation,
-        find and return the state sequence that generated
-        the output sequence, using the forward algorithm.
-        """
+        rows, cols = (len(self.transitions), len(observation))
+        matrix = [[0.0 for i in range(cols)] for j in range(rows)]
 
-    ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
+        matrix[0][0] = 1.0
+
+        state_num = 0
+        for state in self.transitions.keys():
+            trans = self.transitions[observation[0]]
+
+            if observation[1] not in self.emissions[state]:
+                emission = 0
+            else:
+                emission = self.emissions[state][observation[1]]
+
+            matrix[state_num][1] = trans[state] * emission
+
+            state_num += 1
+
+        for i in range(2, len(observation)):
+            state_num = 0
+            for state in self.transitions.keys():
+                sum = 0
+                s2_num = 0
+                for s2 in self.transitions.keys():
+                    if observation[i] not in self.emissions[state]:
+                        emission_prob = 0
+                    else:
+                        emission_prob = self.emissions[state][observation[i]]
+
+                    transition_prob = self.transitions[s2][state]
+
+                    sum += matrix[s2_num][i-1]*transition_prob*emission_prob
+                    s2_num += 1
+
+                matrix[state_num][i] = sum
+
+                state_num += 1
+
+        final_vals = [row[-1] for row in matrix]
+        states = list(self.transitions.keys())
+        max_val, max_state = final_vals[0], states[0]
+        for i in range(1, len(final_vals)):
+            if max_val < final_vals[i]:
+                max_val = final_vals[i]
+                max_state = states[i]
+
+        return max_state
+
+
+    ## you do this: Implement the Viterbi algorithm. Given an Observation (a list of outputs or emissions)
     ## determine the most likely sequence of states.
-
     def viterbi(self, observation):
         """given an observation,
         find and return the state sequence that generated
         the output sequence, using the Viterbi algorithm.
         """
 
+    def printMatrix(self, matrix):
+        keys = list(self.transitions.keys())
+        for i in range(len(self.transitions)):
+            print(keys[i], matrix[i])
+
+
 if __name__ == "__main__":
     model = HMM()
-    model.load('two_english')
+    model.load('partofspeech.browntags.trained')
 
-    print(model.transitions)
+    observ = ['#', 'i', 'shot', 'the', 'elephant', '.']
+    observations = getObservations('ambiguous_sents.obs')
 
-    print(model.generate(6))
+    print(observations)
+    for observation in observations:
+        print('final state:', model.forward(observation))
+
+    # print('final state:', model.forward(observation))
 
 
 
